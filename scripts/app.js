84d0d48385d81567b0f5f1280563af1660e9e90b
mallory13 <mallory.steele13@gmail.com>
2016-02-04 11:42:29 -0500

    added html files

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,11 @@ var projects = "";
     //code block being called by this function
     console.log("App started...");
     
+    //named function way
+    function replaceFirstParagraph(){
+        
+    }
+    
     var firstParagraph;
     firstParagraph= document.getElementById("firstParagraph");
     firstParagraph.innerHTML = "My new paragraph data";
