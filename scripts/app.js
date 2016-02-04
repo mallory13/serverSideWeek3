df77020bc9a608e6738c8ba142de65876db4d068
mallory13 <mallory.steele13@gmail.com>
2016-02-04 11:15:44 -0500

    Added navbar

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//iife immediately invoked function expression
+//annonymous self executed functions
+(function(){
+    //code block being called by this function
+    console.log("App started...");
+    
+})();
