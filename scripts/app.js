8bd11e962b2bcaf9b84bb53bc6e055eda8f59de1
mallory13 <mallory.steele13@gmail.com>
2016-02-04 13:41:39 -0500

    Added main setup to html pages

--- scripts/app.js
+++ scripts/app.js
@@ -9,17 +9,33 @@ var projects = "";
 
 //iife immediately invoked function expression
 //annonymous self executed functions
-(function(){
+(function () {
     //code block being called by this function
     console.log("App started...");
     
     //named function way
-    function replaceFirstParagraph(){
+    function replaceFirstParagraph() {
         
+        console.log("inside replaceFirstParagraph function");
+        var firstParagraph;
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "My new paragraph data";
+
     }
     
-    var firstParagraph;
-    firstParagraph= document.getElementById("firstParagraph");
-    firstParagraph.innerHTML = "My new paragraph data";
-    
+    //declare anonymous function with named alias
+    /*
+    var replaceFirstParagraph = function(){
+        var firstParagraph;
+        
+        firstParagraph = document.getElementByID("first paragraph");
+        firstParagraph.innerHTML = "first para data";
+        
+    };
+*/
+
+
+//call the replaceFirstParagraph function
+replaceFirstParagraph();
+
 })();
