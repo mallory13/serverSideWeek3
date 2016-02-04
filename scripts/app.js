02329ac818d572c7afc2cfb33955ffbde0b85b59
mallory13 <mallory.steele13@gmail.com>
2016-02-04 11:38:15 -0500

    Injected javascript string in html and fixed navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,20 @@
 "use strict";
 
+//global variables
+var missionStatement = "";
+//projects array
+var projects = "";
+
+
+
 //iife immediately invoked function expression
 //annonymous self executed functions
 (function(){
     //code block being called by this function
     console.log("App started...");
+    
+    var firstParagraph;
+    firstParagraph= document.getElementById("firstParagraph");
+    firstParagraph.innerHTML = "My new paragraph data";
     
 })();
